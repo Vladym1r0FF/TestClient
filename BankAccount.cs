3e5be15487458bff411ab9749442dd48d2ad83fb
Vladym1r0FF <vkalpakchiew@gmail.com>
2019-04-20 11:41:58 +0300

    new files has been added to the program

---
+++ BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Client
+{
+    class BankAccount
+    {
+        private int id;
+        private double balance;
+
+        public BankAccount (int id)
+        {
+            this.id = id;
+        }
+        public double Balance => balance;
+        public int ID => id;
+        public void Deposit(double amount)
+        {
+            this.balance += amount;
+        }
+        public void Withdraw (double amount)
+        {
+            if (amount > this.balance)
+            {
+                throw new InvalidOperationException("Insufficient Balance");
+            }
+            this.balance -= amount;
+        }
+        public override string ToString()
+        {
+            return $"Account ID {this.id}, balance {this.Balance:F2}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var parts = command.Split(' ');
+                var action = parts[0];
+                var id = int.Parse(parts[1]);
+
+                double amount = 0;
+
+                if (parts.Length > 2)
+                {
+                    amount = double.Parse(parts[2]);
+                }
+
+
+                try
+                {
+                    switch (action)
+                    {
+                        case "Create":
+                            AccountManager.Create(id);
+                            break;
+                        case "Deposit":
+                            AccountManager.Deposit(id, amount);
+                            break;
+                        case "Withdraw":
+                            AccountManager.Withdraw(id, amount);
+                            break;
+                        case "Print":
+                            var accountInformation = AccountManager.GetAccountInformation(id);
+                            Console.WriteLine(accountInformation);
+                            break;
+                    }
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+
+    }
+}
